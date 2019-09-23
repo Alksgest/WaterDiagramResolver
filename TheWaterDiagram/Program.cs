c5507d1f70242a5fab00ead45babb80148a61a56
vlky <vlky@isd.dp.ua>
2019-09-23 11:34:03 +0300

    fixes

--- TheWaterDiagram/Program.cs
+++ TheWaterDiagram/Program.cs
@@ -13,14 +13,14 @@ namespace TheWaterDiagram
 
         public static int[] data2 = { 4, 3, 4, 7, 6, 7 };
 
-        public static string sampleString = "5, 2, 1, 1, 0, 1, 1, 2, 2, 4";
+        public static string sampleString = "5,   . . . . .... .  .   2, 1, 1, 0, 1, 1, 2, 2, 4";
 
         private static string[] TokeniseString(string str)
         {
             return str.Replace(" ", "").Split(new char[] { ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
         }
 
-        private static int[] ParseAlLValues(string[] args)
+        private static int[] ParseValues(string[] args)
         {
             if (args.Length < 2)
                 throw new ArgumentException("There are no input data");
@@ -32,7 +32,7 @@ namespace TheWaterDiagram
 
         static void Main(string[] args)
         {
-            var argsData = ParseAlLValues(new string[] { "path", sampleString });
+            var argsData = ParseValues(new string[] { "path", sampleString });
 
             IWaterDiagramResolver resolver = new WaterDiagramResolver(argsData);
 
